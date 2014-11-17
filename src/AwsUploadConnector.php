a64893864eda6600b28fff3a088b39b425d040dc
onysko <pavlo.onysko@gmail.com>
2014-11-17 22:49:07 +0200

    module created

---
+++ src/AwsUploadConnector.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: onysko
+ * Date: 17.11.2014
+ * Time: 22:46
+ */
+
+namespace samson\upload;
+
+
+class AwsUploadConnector extends CompressableExternalModule {
+    /** Идентификатор модуля */
+    protected $id = 'samsonuploadaws';
+} 
