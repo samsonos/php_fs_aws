e3de2950a134744ca6ce6416690500998c85e51e
onysko <pavlo.onysko@gmail.com>
2014-12-15 14:48:38 +0200

    module connector added

---
+++ src/AWSConnector.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: onysko
+ * Date: 15.12.2014
+ * Time: 14:38
+ */
+
+namespace samson\fs;
+
+
+class AWSConnector extends \samson\core\CompressableExternalModule {
+    /** @var string Module identifier */
+    public $id = 'fs_aws';
+}
